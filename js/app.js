9674af230fae6395cdcf942fc7c30ac4cf68f071
vasudevapitta <tizzicboy@gmail.com>
2018-08-21 16:59:22 -0400

    added API to get crypto prices
    Implemented crypto live prices from CoinMarketCap API using AJAX jQuery. Added functionality for widgets to convert prices to various coins.

--- js/app.js
+++ js/app.js
@@ -1,87 +1,166 @@
 'use strict';
 
-/*=====SIDE NAVIGATION BAR FOR MOBILE/IPAD & NARROW VIEWPORTS ON DESKTOP=====*/
+let coins = {
+  bitcoin: {
+    id: 1,
+  },
+  ethereum: {
+    id: 1027,
+  },
+  ethereumClassic: {
+    id: 1321,
+   },
+  bitcoinCash: {
+    id: 1831,
+  },
+};
 
-const hamBtn = $(".fa-bars");
-const sideNav = $(".side-nav");
-const overlay = $(".overlay");
 
-hamBtn.click(function(){
-	sideNav.toggleClass("slide");
-	sideNav.focus();
-	overlay.show();
+$.ajax({
+  url: `https://api.coinmarketcap.com/v2/ticker/?limit=13&sort=coins`  
+}).done(function(response){
+  $(response.data).each(function(ind, item, arr){
+   	coins.bitcoin.price = item[coins.bitcoin.id].quotes.USD.price;
+    coins.ethereum.price = item[coins.ethereum.id].quotes.USD.price;
+    coins.ethereumClassic.price = item[coins.ethereumClassic.id].quotes.USD.price;
+    coins.bitcoinCash.price = item[coins.bitcoinCash.id].quotes.USD.price;
+
+    init(coins.bitcoin.price, coins.ethereum.price, coins.ethereumClassic.price, coins.bitcoinCash.price);
+  });
+})
+.fail(function(){
+  console.log('There was an error');
 });
 
-overlay.click(function(){
-	sideNav.toggleClass("slide");
-	$(this).hide();
-});
+function init(btc, eth, ethC, btcC){
 
-/*=================SET PRICES HERE=====================*/
+		let fixedNum = 4;
 
-const pwrclr1yr = 8999;
-const pwrclr2yr = 9999;
+		/*=====SIDE NAVIGATION BAR FOR MOBILE/IPAD & NARROW VIEWPORTS ON DESKTOP=====*/
 
-const ggbyt1yr = 4889;
-const ggbyt2yr = 5589;
+		const hamBtn = $(".fa-bars");
+		const sideNav = $(".side-nav");
+		const overlay = $(".overlay");
 
-const msi1yr = 4889;
-const msi2yr = 5589;
+		hamBtn.click(function(){
+			sideNav.toggleClass("slide");
+			sideNav.focus();
+			overlay.show();
+		});
 
-const xfx1yr = 4889;
-const xfx2yr = 5589;
+		overlay.click(function(){
+			sideNav.toggleClass("slide");
+			$(this).hide();
+		});
 
-const pwrclr1price = $('#pwr1price');
-const pwrclr2price = $('#pwr2price');
+		/*=================SET PRICES HERE=====================*/
 
-const ggbyt1price = $('#ggbyt1price');
-const ggbyt2price = $('#ggbyt2price');
+		const pwrclr1yr = 8999;
+		const pwrclr2yr = 9999;
 
-const msi1price = $('#msi1price');
-const msi2price = $('#msi2price');
+		const ggbyt1yr = 4889;
+		const ggbyt2yr = 5589;
 
-const xfx1price = $('#xfx1price');
-const xfx2price = $('#xfx2price');
+		const msi1yr = 4889;
+		const msi2yr = 5589;
 
-const domPriceElems = [pwrclr1price, pwrclr2price, ggbyt1price, ggbyt2price, msi1price, msi2price, xfx1price, xfx2price];
-const setPrices = [pwrclr1yr, pwrclr2yr, ggbyt1yr, ggbyt2yr, msi1yr, msi2yr, xfx1yr, xfx2yr];
+		const xfx1yr = 4889;
+		const xfx2yr = 5589;
 
-//appending prices to the DOM
-$(domPriceElems).each(function(ind, arrVal){
-	$(arrVal).html(setPrices[ind]);
-});
+		const pwrclr1price = $('#pwr1price');
+		const pwrclr2price = $('#pwr2price');
 
-//for all input number elements
-$(':input[type="number"]').on({
-	click: function(){
-		updateVal.call(this);
-	},
+		const ggbyt1price = $('#ggbyt1price');
+		const ggbyt2price = $('#ggbyt2price');
 
-	keypress: function(e){
-		if(e.keyCode===13){
-			updateVal.call(this);
+		const msi1price = $('#msi1price');
+		const msi2price = $('#msi2price');
+
+		const xfx1price = $('#xfx1price');
+		const xfx2price = $('#xfx2price');
+
+		const domPriceElems = [pwrclr1price, pwrclr2price, ggbyt1price, ggbyt2price, msi1price, msi2price, xfx1price, xfx2price];
+		const setPrices = [pwrclr1yr, pwrclr2yr, ggbyt1yr, ggbyt2yr, msi1yr, msi2yr, xfx1yr, xfx2yr];
+
+		//appending prices to the DOM
+		$(domPriceElems).each(function(ind, arrVal){
+			$(arrVal).html(setPrices[ind]);
+		});
+
+		//for all input number elements
+		$(':input[type="number"]').on({
+			click: function(){
+				updateVal.call(this);
+			},
+
+			keypress: function(e){
+				if(e.keyCode===13){
+					updateVal.call(this);
+				}
+			},
+
+			focusout: function(){
+				updateVal.call(this);
+			}
+		});
+
+		function updateVal(){
+			const obj = this;
+			const inputVal = $(obj).val();
+			const total = $(obj).parent().siblings('td').last().children('.total');
+			const category = $(obj).parents('table').find('.category').val();
+			const num = $(obj).parent().siblings('.price').html();
+			convertToCoin.apply(this, [obj, inputVal, total, category, num]);
+			updateFinalVal.call(this);
 		}
-	},
 
-	focusout: function(){
-		updateVal.call(this);
-	}
-});
+		function convertToCoin(obj, inputVal, total, category, num){
+			
+			if(category==='usd'){
+				total.html(inputVal*num);
+				fixedNum = 2;
+			}
 
-function updateVal(){
-	const obj = $(this);
-	const inputVal = obj.val();
-	const num = obj.parent().siblings('.price').html();
-	const total = obj.parent().siblings('td').last().children('.total');
-	total.html(inputVal*num);
-	updateFinalVal.call(this);
-}
+			else if(category==='btc'){
+				const convertedCoinPrice = ((inputVal*num)/btc).toFixed(4);
+				total.html(convertedCoinPrice);
+				fixedNum = 4;
+			}
 
-function updateFinalVal(){
-	const obj = $(this);
-	const objLookUp = obj.closest('table').find('.total');
-	const total1 = parseInt(objLookUp.eq(0).html());
-	const total2 = parseInt(objLookUp.eq(1).html());
-	const finalTotal = obj.closest('table').find('.finaltotal');
-	finalTotal.html(total1+total2);
+			else if(category==='bch'){
+				const convertedCoinPrice = ((inputVal*num)/btcC).toFixed(4);
+				total.html(convertedCoinPrice);
+				fixedNum = 4;
+			}
+
+			else if(category==='eth'){
+				const convertedCoinPrice = ((inputVal*num)/eth).toFixed(4);
+				total.html(convertedCoinPrice);
+				fixedNum = 4;
+			}
+
+			else if(category==='etc'){
+				const convertedCoinPrice = ((inputVal*num)/ethC).toFixed(4);
+				total.html(convertedCoinPrice);
+				fixedNum = 4;
+			}
+
+		}
+
+			$('select').on('change', function(){
+				const input = $(this).parents('tr').siblings('tr').find(':input[type="number"]').eq(0);
+				updateVal.call(input);
+				const input1 = $(this).parents('tr').siblings('tr').find(':input[type="number"]').eq(1);
+				updateVal.call(input1);
+			});
+
+		function updateFinalVal(){
+			const obj = $(this);
+			const objLookUp = obj.closest('table').find('.total');
+			const total1 = Number(objLookUp.eq(0).html());
+			const total2 = Number(objLookUp.eq(1).html());
+			const finalTotal = obj.closest('table').find('.finaltotal');
+			finalTotal.html((total1+total2).toFixed(fixedNum));
+		}
+
 }
